2a74aa1cbc6e26fc758c2fe8dd82de53ecbec25e
Mia Kamenetski <mia.kamenetski@online.de>
2022-11-28 10:36:58 +0100

    Grandparent

---
+++ com/cc/java/Child.java
@@ -0,0 +1,11 @@
+package com.cc.java;
+
+import javax.print.attribute.standard.OutputDeviceAssigned;
+
+public class Child extends Parent{
+
+    // public String sayHello() {
+    //     return "Hello from Child!";
+    // }
+    
+}
